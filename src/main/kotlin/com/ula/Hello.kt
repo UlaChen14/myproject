75d26f578a0c266ccd7e422144deb7e1a32983f5
Ula <ulachen14@gmail.com>
2019-05-15 14:06:56 +0800

    Java and Kotlin variable, data type

--- src/main/kotlin/com/ula/Hello.kt
+++ src/main/kotlin/com/ula/Hello.kt
@@ -5,6 +5,12 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age : Int = 19
+    age = 11
+    var weight = 88.2
+    var name : String
+    name = "Ula"
+    print(name)
 }
 
 class Human {
